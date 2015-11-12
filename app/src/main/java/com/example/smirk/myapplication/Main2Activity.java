a8d50f32df37ac15da4d5242e815bfed826d8a43
TestSmirk <d.kc@qq.com>
2015-11-12 21:45:53 +0800

    e

---
+++ app/src/main/java/com/example/smirk/myapplication/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.smirk.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/example/smirk/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.smirk.myapplication;
+
+import android.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
